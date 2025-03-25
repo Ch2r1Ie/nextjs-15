bacc653af0178da6ea39b427b3bebecb15517141
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-03-26 00:05:24 +0700

    v1.0.11

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const PricingCard = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center p-6">
       <PricingSectionCards />
     </div>
   );
