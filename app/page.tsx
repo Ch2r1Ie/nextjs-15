1645e70c71085bb1e1c42827a3f667cd772cce1a
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-03-26 16:17:04 +0700

    v1.0.31

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import HeroSectionSimpleCentred from "@/components/layout/simple-centre";
 import Team05Page from "@/components/layout/team-section";
 import FeatureSectionSimple from "@/components/layout/feature-section";
+import CardGroup from "@/components/layout/stats";
+import { Logos3 } from "@/components/layout/logos";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen py-6 px-6 scroll-smooth">
       <HeroSectionSimpleCentred />
       <FeatureSectionSimple />
+      <CardGroup />
+      <Logos3 />
       <Team05Page />
     </div>
   );
