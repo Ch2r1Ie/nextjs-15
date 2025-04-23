9a186dc1171f616e93dbc180ed105628a070dc5d
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-04-23 15:17:45 +0700

    v1.0.55

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,12 +1,17 @@
+import Image from "next/image";
 import { LoginForm } from "@/components/layout/login-form";
 
 export default function Login() {
   return (
-    <div
-      className="flex min-h-svh w-full items-center justify-center bg-cover bg-center p-6 md:p-10"
-      style={{ backgroundImage: "url('/assets/auth-background.jpg')" }}
-    >
-      <div className="w-full max-w-md rounded-xl p-6 shadow-xl backdrop-blur-sm">
+    <div className="relative flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <Image
+        src="/assets/auth-background.jpg"
+        alt="Background"
+        fill
+        className="-z-10 object-cover object-center"
+        priority
+      />
+      <div className="w-full max-w-md rounded-xl bg-white/80 p-6 shadow-xl backdrop-blur-sm">
         <LoginForm />
       </div>
     </div>
