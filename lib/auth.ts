e2712cf9de359ab0ed2c2f60552425155c9233a1
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-04-24 15:52:59 +0700

    v1.0.64

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,18 +1,10 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+import Discord from "next-auth/providers/discord";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
   secret: process.env.AUTH_SECRET,
-  session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60,
-  },
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-      allowDangerousEmailAccountLinking: true,
-    }),
-  ],
+  providers: [Google, Discord, Github],
 });
