a6ca91f61ac943ef83a66dbdeb18ad1c5d718164
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-04-28 22:26:18 +0700

    v1.0.96

--- actions/(auth)/use-auth.ts
+++ actions/(auth)/use-auth.ts
@@ -10,5 +10,33 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60,
   },
-  providers: [Google, Github, Discord],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+        },
+      },
+    }),
+    Github({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+        },
+      },
+    }),
+    Discord({
+      clientId: process.env.AUTH_DISCORD_ID,
+      clientSecret: process.env.AUTH_DISCORD_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+        },
+      },
+    }),
+  ],
 });
