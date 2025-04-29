566ba092e79246cd6fe6a44e9e6dba9dc7d99886
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-04-29 14:43:39 +0700

    v1.0.99

--- components/layout/login/login-form.tsx
+++ components/layout/login/login-form.tsx
@@ -15,7 +15,6 @@ import {
   handleGithubSignIn,
   handleDiscordSignIn,
 } from "@/actions/(auth)/use-providerSignIn";
-import { motion } from "framer-motion";
 import Link from "next/link";
 import { IoChevronBackOutline } from "react-icons/io5";
 
@@ -35,80 +34,73 @@ const popPreset = {
 
 export function LoginForm() {
   return (
-    <motion.div
-      variants={popPreset}
-      initial="initial"
-      animate="animate"
-      className="w-full max-w-md rounded-xl p-2 shadow-xl backdrop-blur-md"
-    >
-      <div className="flex flex-col gap-6">
-        <Card>
-          <CardHeader>
-            <CardTitle className="flex items-center gap-x-2 text-2xl">
-              <Link href="/home" className="rounded-md hover:bg-gray-100">
-                <IoChevronBackOutline />
-              </Link>
-              <span>Log in or Sign in</span>
-            </CardTitle>
-            <CardDescription>
-              Use your email or another service to continue with
-              MyCompany&apos;s (it&apos;s free)!
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="flex w-full justify-center">
-              <div className="w-11/12">
-                <div className="space-y-2">
-                  <Button
-                    variant="outline"
-                    className="w-full cursor-pointer"
-                    type="button"
-                    onClick={async () => {
-                      await handleGoogleSignIn();
-                    }}
-                  >
-                    <FcGoogle className="mr-2 h-6 w-6" />
-                    <span>Continue with Google</span>
-                  </Button>
-                  <Button
-                    variant="outline"
-                    className="w-full cursor-pointer"
-                    type="button"
-                    onClick={async () => {
-                      await handleGithubSignIn();
-                    }}
-                  >
-                    <FaGithub className="mr-2 h-6 w-6" />
-                    <span>Continue with GitHub</span>
-                  </Button>
-                  <Button
-                    variant="outline"
-                    className="w-full cursor-pointer"
-                    type="button"
-                    onClick={async () => {
-                      await handleDiscordSignIn();
-                    }}
-                  >
-                    <FaDiscord className="mr-2 h-6 w-6 text-[#5865F2]" />
-                    <span>Continue with Discord</span>
-                  </Button>
-                </div>
+    <div className="flex w-full max-w-md flex-col gap-6 rounded-xl p-2 shadow-xl backdrop-blur-md">
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex items-center gap-x-2 text-2xl">
+            <Link href="/home" className="rounded-md hover:bg-gray-100">
+              <IoChevronBackOutline />
+            </Link>
+            <span>Log in or Sign in</span>
+          </CardTitle>
+          <CardDescription>
+            Use your email or another service to continue with MyCompany&apos;s
+            (it&apos;s free)!
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex w-full justify-center">
+            <div className="w-11/12">
+              <div className="space-y-2">
+                <Button
+                  variant="outline"
+                  className="w-full cursor-pointer"
+                  type="button"
+                  onClick={async () => {
+                    await handleGoogleSignIn();
+                  }}
+                >
+                  <FcGoogle className="mr-2 h-6 w-6" />
+                  <span>Continue with Google</span>
+                </Button>
+                <Button
+                  variant="outline"
+                  className="w-full cursor-pointer"
+                  type="button"
+                  onClick={async () => {
+                    await handleGithubSignIn();
+                  }}
+                >
+                  <FaGithub className="mr-2 h-6 w-6" />
+                  <span>Continue with GitHub</span>
+                </Button>
+                <Button
+                  variant="outline"
+                  className="w-full cursor-pointer"
+                  type="button"
+                  onClick={async () => {
+                    await handleDiscordSignIn();
+                  }}
+                >
+                  <FaDiscord className="mr-2 h-6 w-6 text-[#5865F2]" />
+                  <span>Continue with Discord</span>
+                </Button>
+              </div>
 
-                <div className="text-muted-foreground mt-4 text-center text-sm">
-                  {"By continuing, you agree to MyCompany's "}
-                  <span className="cursor-pointer text-blue-800 underline underline-offset-4 hover:text-blue-500">
-                    <Link href="/">Terms of Use </Link>
-                  </span>
-                  and
-                  <span className="cursor-pointer text-blue-800 underline underline-offset-4 hover:text-blue-500">
-                    <Link href="/"> Privacy Policy.</Link>
-                  </span>
-                </div>
+              <div className="text-muted-foreground mt-4 text-center text-sm">
+                {"By continuing, you agree to MyCompany's "}
+                <span className="cursor-pointer text-blue-800 underline underline-offset-4 hover:text-blue-500">
+                  <Link href="/">Terms of Use </Link>
+                </span>
+                and
+                <span className="cursor-pointer text-blue-800 underline underline-offset-4 hover:text-blue-500">
+                  <Link href="/"> Privacy Policy.</Link>
+                </span>
               </div>
             </div>
-          </CardContent>
-        </Card>
-      </div>
-    </motion.div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
