8d6798c9310916dee7377e3be72cd76ad83f2fc3
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-05-03 22:24:42 +0700

    v1.0.119

--- components/layout/home/menubar.tsx
+++ components/layout/home/menubar.tsx
@@ -10,6 +10,7 @@ export function MenubarDemo() {
 
   return (
     <div>
+      {/* Desktop Menu */}
       <div className="hidden space-x-4 md:flex">
         <Button
           variant="ghost"
@@ -23,26 +24,30 @@ export function MenubarDemo() {
         >
           Features
         </Button>
-        <Link href="/pricing">
+        <Link href="/pricing" passHref>
           <Button
             variant="ghost"
             className="rounded-2xl px-4 py-6 text-xl text-white transition-all duration-300 hover:cursor-pointer hover:bg-white hover:text-black"
           >
             Pricing
           </Button>
-          <span className="mx-5 self-center text-xl text-white">|</span>
-          <Link href="/login">
-            <Button className="rounded-2xl bg-gradient-to-r from-amber-400 via-yellow-500 to-orange-500 px-4 py-6 text-xl text-white transition-all duration-300 hover:cursor-pointer hover:bg-white hover:text-black">
-              Start For Free
-            </Button>
-          </Link>
+        </Link>
+        <span className="mx-5 self-center text-xl text-white">|</span>
+        <Link href="/login" passHref>
+          <Button className="rounded-2xl bg-gradient-to-r from-amber-400 via-yellow-500 to-orange-500 px-4 py-6 text-xl text-white transition-all duration-300 hover:cursor-pointer hover:bg-white">
+            Start For Free
+          </Button>
         </Link>
       </div>
+
+      {/* Mobile Toggle */}
       <div className="md:hidden">
         <Button variant="ghost" onClick={() => setIsOpen(!isOpen)}>
           {isOpen ? <X className="size-md" /> : <Menu className="size-md" />}
         </Button>
       </div>
+
+      {/* Mobile Menu */}
       {isOpen && (
         <div className="absolute left-1/2 z-50 mt-4 w-full max-w-xs -translate-x-1/2 rounded-3xl bg-white/70 p-6 shadow-lg ring-1 ring-black/10 backdrop-blur-lg md:hidden">
           <div
@@ -57,15 +62,19 @@ export function MenubarDemo() {
           >
             Features
           </div>
-          <Link href="/pricing" onClick={() => setIsOpen(false)}>
-            <div className="mb-2 cursor-pointer rounded-xl px-6 py-2 text-center text-xl font-semibold text-black transition-all duration-300 ease-in-out hover:scale-105 hover:bg-blue-100">
-              Pricing
-            </div>
+          <Link
+            href="/pricing"
+            onClick={() => setIsOpen(false)}
+            className="mb-2 block rounded-xl px-6 py-2 text-center text-xl font-semibold text-black transition-all duration-300 ease-in-out hover:scale-105 hover:bg-blue-100"
+          >
+            Pricing
           </Link>
-          <Link href="/login" onClick={() => setIsOpen(false)}>
-            <div className="cursor-pointer rounded-xl px-6 py-2 text-center text-xl font-semibold text-black transition-all duration-300 ease-in-out hover:scale-105 hover:bg-blue-100">
-              Start For Free
-            </div>
+          <Link
+            href="/login"
+            onClick={() => setIsOpen(false)}
+            className="block rounded-xl px-6 py-2 text-center text-xl font-semibold text-black transition-all duration-300 ease-in-out hover:scale-105 hover:bg-blue-100"
+          >
+            Start For Free
           </Link>
         </div>
       )}
