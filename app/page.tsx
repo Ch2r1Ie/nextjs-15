a37d078b3a50b9996b40354a426eace1b7149053
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-05-04 13:40:09 +0700

    v1.0.123

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ import { MenubarDemo } from "@/components/layout/landing/menubar";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center overflow-x-hidden scroll-smooth px-6 py-6">
-      <div className="fixed inset-x-0 top-2 left-1/2 z-50 mx-auto mt-2 flex w-11/12 -translate-x-1/2 items-center justify-between rounded-2xl bg-gradient-to-b from-gray-800 via-gray-900 to-black px-5 py-2 text-white shadow-2xl transition-shadow duration-100 hover:bg-gray-800 hover:bg-gradient-to-r hover:text-green-500 sm:mx-2 xl:mt-5">
-        <span className="flex-grow px-2 text-xl font-bold hover:cursor-pointer xl:text-3xl">
-          .AI
+      <div className="fixed inset-x-0 top-2 left-1/2 z-50 mx-auto mt-2 flex w-11/12 -translate-x-1/2 items-center justify-between rounded-2xl bg-gradient-to-r from-pink-500 via-yellow-400 to-blue-500 px-5 py-2 text-white shadow-2xl transition-shadow duration-100 hover:bg-gray-800 hover:bg-gradient-to-r hover:text-green-500 sm:mx-2 xl:mt-5">
+        <span className="flex items-center gap-2 px-2 text-2xl font-bold hover:cursor-pointer xl:text-3xl">
+          .World
         </span>
         <MenubarDemo />
       </div>
--- components/layout/landing/simple-centre.tsx
+++ components/layout/landing/simple-centre.tsx
@@ -9,7 +9,7 @@ export default function HeroSectionSimpleCentred() {
         <div className="container mt-15 lg:py-32">
           <div className="mx-auto mt-5 max-w-2xl text-center">
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-              Let&apos;s Build Together
+              Let&apos;s Build Your World
             </h1>
           </div>
           <div className="mx-auto mt-5 max-w-3xl text-center">
