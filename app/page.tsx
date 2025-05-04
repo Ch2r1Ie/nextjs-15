a93abe65ab4f8b79fe99793625cc42db5dc2037c
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-05-04 13:58:18 +0700

    v1.0.128

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { MenubarDemo } from "@/components/layout/landing/menubar";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center overflow-x-hidden scroll-smooth px-6 py-6">
-      <div className="fixed inset-x-0 top-2 left-1/2 z-50 mx-auto mt-5 flex w-11/12 -translate-x-1/2 items-center justify-between rounded-2xl bg-gradient-to-r from-green-600 via-blue-400 to-indigo-700 px-5 py-2 text-white shadow-2xl transition-shadow duration-100 hover:bg-gray-800 hover:bg-gradient-to-r sm:mx-2">
+      <div className="fixed inset-x-0 top-2 left-1/2 z-50 mx-auto mt-5 flex w-11/12 -translate-x-1/2 items-center justify-between rounded-2xl bg-gradient-to-r from-green-600 via-blue-400 to-indigo-700 px-5 py-2 text-white shadow-2xl transition-shadow duration-100 hover:bg-gray-800 hover:bg-gradient-to-r sm:mx-2 xl:w-3/4">
         <span className="flex items-center gap-2 px-2 text-2xl font-bold hover:cursor-pointer xl:text-3xl">
           .World
         </span>
