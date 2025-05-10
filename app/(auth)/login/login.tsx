d6a01cc7ed12f85d25966e02e380400b1645c0ba
Thannathon Nuengpud <a667371@AR8040667371N1.local>
2025-05-10 23:56:27 +0700

    v1.0.171

--- app/(auth)/login/login.tsx
+++ app/(auth)/login/login.tsx
@@ -2,6 +2,7 @@ import { LoginForm } from "@/components/layout/login/login-form";
 
 export default function Login() {
   return (
+    
     <div className="relative flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <LoginForm isReturnLink={true} />
     </div>
